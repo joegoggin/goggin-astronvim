1d011784c568b3bc30b9cce523c0c353dc682a25
joegoggin <joegoggin27@gmail.com>
2023-04-25 16:48:48 -0400

    update colorscheme

--- init.lua
+++ init.lua
@@ -17,7 +17,7 @@ return {
     },
   },
   -- Set colorscheme to use
-  colorscheme = "carbonfox",
+  colorscheme = "everforest",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
