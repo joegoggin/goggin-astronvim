06f55b6b8590b9f0f551d3071ded9a1924d743aa
joegoggin <joegoggin27@gmail.com>
2023-04-25 04:34:08 -0400

    add language support defaults and add key mappings

--- mappings.lua
+++ mappings.lua
@@ -6,12 +6,26 @@
 return {
   -- first key is the mode
   n = {
+    -- Move code
+    ["<M-j>"] = { "<cmd>m +1<cr>", desc = "Move code down one line" },
+    ["<M-k>"] = { "<cmd>m -2<cr>", desc = "Move code up on line" },
+    -- Buffers
+    ["L"] = {
+      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+      desc = "Next Buffer",
+    },
+    ["H"] = {
+      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+      desc = "Previous Buffer",
+    },
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
